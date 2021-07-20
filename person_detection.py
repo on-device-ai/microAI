c6df34a27c3e309d006615c4aeb6062f6503be98
Yi-Lin Tung <yilintung@on-device-ai.com>
2021-07-20 16:31:25 +0800

    Update person_detection.py

--- person_detection.py
+++ person_detection.py
@@ -15,14 +15,15 @@ img_w    = 96
 img_h    = 96
 img_ch   = 1
 img_size = img_w * img_h * img_ch
-img_buff = array.array('b',[0] * img_size)
+
+input_tensor  = array.array('b',[0] * img_size)
+output_tensor = array.array('b',[0,0])
   
 while True :
-  microai.get_image(img_w,img_h,img_ch,img_buff)
-  microai.set_input_tensor_value(img_buff)
+  microai.get_image(img_w,img_h,img_ch,input_tensor)
+  microai.set_input_tensor_value(input_tensor)
   if tflite.interpreter_invoke() is False :
     sys.exit()
-  output_tensor = array.array('b',[0,0])
   microai.get_output_tensor_value(output_tensor) 
   person_score = output_tensor[1]
   no_person_score = output_tensor[0]
