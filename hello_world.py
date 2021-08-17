7ab52ea84bbf6edd242f2ee6b962b701e052bf78
Yi-Lin Tung <yilintung@on-device-ai.com>
2021-08-17 20:47:21 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -13,19 +13,26 @@ if microai.load_hello_world_model() is False :
 tflite.build_interpreter()
 if tflite.allocate_tensors() is False :
   sys.exit()
+
+input_tensor  = array.array('b', [0])
+output_tensor = array.array('b',[0])
   
 inference_count = 0;
 
 while True :
+  
   position = float(inference_count) / float(inferences_per_cycle)
   x = position * x_range
-  input_tensor = array.array('b',[microai.value_input_quantize(x)])
+  
+  input_tensor[0] = microai.value_input_quantize(x)
   microai.set_input_tensor_value(input_tensor)
+  
   if tflite.interpreter_invoke() is False :
     sys.exit()
-  output_tensor = array.array('b',[0])
+  
   microai.get_output_tensor_value(output_tensor)
   y = microai.value_output_dequantize(output_tensor[0])
+  
   inference_count += 1
    
   result_str = " x = %f , y = %f " % (x,y)
